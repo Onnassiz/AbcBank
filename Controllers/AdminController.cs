0baa16a5a787a4e7ff0d5d2f930dcf083073e118
Onnassiz <onnassiz@gmail.com>
2017-01-17 05:51:36 +0000

    still working on adding admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -34,9 +34,32 @@ namespace AbcBank.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string Type)
         {
-            return View();
+            var result = _context.Persons.OfType<Administrator>().Join(_context.BankBranches,
+                    x => x.BankBranchId, d => d.Id,
+                    (person, branch) => new
+                    {
+                        person, branch
+                    }
+            ).OrderByDescending(i => i.person.DateUpdated);
+
+            List<PersonJoinView> model = new List<PersonJoinView>();
+
+            foreach (var item in result)
+            {
+                model.Add(new PersonJoinView()
+                {
+                    Id = item.person.Id,
+                    FullName = item.person.FullName,
+                    Email = item.person.Email,
+                    AdminType = item.person.AdminType,
+                    Sex = item.person.Sex,
+                    HireDate = item.person.HireDate,
+                    BranchName = item.branch.BranchName
+                });
+            }
+            return View(model);
         }
 
         [HttpGet]
@@ -78,7 +101,7 @@ namespace AbcBank.Controllers
                     _context.SaveChanges();
                     await CreateAccount(administrator.Email);
                     await SetRole(administrator.Email, administrator.AdminType);
-                    await SendAuthMail(administrator.Email);
+                    await SendAuthMail(administrator.Email, administrator.FullName);
                 }
                 catch (DbUpdateException ex)
                 {
@@ -167,17 +190,68 @@ namespace AbcBank.Controllers
             return new string(chars.ToArray());
         }
 
-        private async Task SendAuthMail(string email)
+        private async Task SendAuthMail(string email, string fullName)
         {
             var user = await _userManager.FindByNameAsync(email);
             var code = await _userManager.GeneratePasswordResetTokenAsync(user);
             var callbackUrl = Url.Action("CreatePassword", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
 
             Mailer.Send(email, "Set Up Password",
-                $"You have been registered as an ABC Bank administrator. <br/><br/>Please click this : <a href='{callbackUrl}'>link</a> to setup your password");
+                $"Hello {fullName},<br/><br/>You have been registered as an ABC Bank administrator. <br/><br/>Please click this : <a href='{callbackUrl}'>link</a> to setup your password");
             TempData["Response"] = "Please check your email.";
         }
 
+        public async Task<IActionResult> Delete(string Id)
+        {
+            var user = _context.Persons.Find(Id);
+            var User = await _userManager.FindByNameAsync(user.Email);
+            await _userManager.DeleteAsync(User);
+            _context.Persons.Remove(user);
+            _context.SaveChanges();
+            TempData["Response"] = "Admin Record Successfully Deleted";
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string Id)
+        {
+            ViewBag.Sex = new List<string> {"Male", "Female"};
+            ViewBag.MarritalStatus = new List<string> {"Single", "Married", "Divorced"};
+
+            List<SelectListItem> AdminType = new List<SelectListItem>();
+            AdminType.Add(new SelectListItem {Text = "Banker", Value = "Banker"});
+            AdminType.Add(new SelectListItem {Text = "Teller", Value = "Teller", Selected = true});
+
+            var Branch = new SelectList(_context.BankBranches.ToList(), "Id", "BranchName");
+
+            ViewBag.AdminType = AdminType;
+            ViewBag.Branch = Branch;
+
+            return View(_context.Persons.Find(Id));
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string Id, Administrator administrator)
+        {
+            ViewBag.Sex = new List<string> {"Male", "Female"};
+            ViewBag.MarritalStatus = new List<string> {"Single", "Married", "Divorced"};
+
+            List<SelectListItem> AdminType = new List<SelectListItem>();
+            AdminType.Add(new SelectListItem {Text = "Banker", Value = "Banker"});
+            AdminType.Add(new SelectListItem {Text = "Teller", Value = "Teller", Selected = true});
+
+            var Branch = new SelectList(_context.BankBranches.ToList(), "Id", "BranchName");
+
+            ViewBag.AdminType = AdminType;
+            ViewBag.Branch = Branch;
+
+            administrator.DateUpdated = DateTime.Now;
+            _context.Persons.Update(administrator);
+            _context.SaveChanges();
+
+            TempData["Response"] = "Record Successfully Updated";
+            return RedirectToAction("Index");
+        }
 
         private void AddErrors(IdentityResult result)
         {
---
+++ Models/PersonJoinView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbcBank.Models
+{
+    public class PersonJoinView
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Sex { get; set; }
+        public string AdminType { get; set; }
+        public DateTime HireDate { get; set; }
+        public string BranchName { get; set; }
+    }
+}
